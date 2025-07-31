4c6d5bbe229416a121b89d46e8feb113f994ea04
Renee Desporte <reneedesporte@Renees-MacBook-Pro.local>
2025-07-30 22:14:23 -0700

    Add method for getting credits associated with content title.

--- src/kaggle_netfix/data.py
+++ src/kaggle_netfix/data.py
@@ -31,3 +31,38 @@ def load(directory):
     titles = pd.read_csv(titles_path)
     credits = pd.read_csv(credits_path)
     return titles, credits
+
+def get_credits(title, titles, credits, content_id=None):
+    """
+    Get the personnel credits associated with the content title.
+
+    Parameters
+    ----------
+    title : string
+        The content title from the `title` column of the content's row.
+    titles : pandas.DataFrame
+        The tabular data with content data.
+    credits : pandas.DataFrame
+        The tabular data with personnel credits.
+    content_id : string, default=None
+        Optional content ID from the `id` column of `titles`.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The subset of the `credits` DataFrame representing the
+         credits associated with `content_id`.
+
+    Raises
+    ------
+    KeyError
+        If `title` isn't unique in `titles` and `content_id`
+         is None.
+    """
+    if content_id is None:
+        content_id = titles[titles["title"] == title]["id"]
+    assert len(content_id) == 1, (
+        f'"{title}" is not a unique title. Please provide a `content_id`.'
+    )
+    content_id = content_id.values[0]
+    return credits[credits["id"] == content_id]
